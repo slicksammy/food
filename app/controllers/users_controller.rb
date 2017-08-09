0c0464372775afc466e47431d901644a5d8f4884
Sam E <sammy@Sams-MacBook-Pro-2.local>
2017-08-08 19:35:54 -0500

    changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < SessionsController
   include CheckoutHelper
 
   before_action :permit_create_user_params, only: :create
-  before_action :redirect_from_login_if_necessary, only: :signup
+  before_action :redirect_from_login_if_necessary, only: [:signup, :reset_password]
   before_action :redirect_to_login_if_neccessary, only: :home
   # before_action :redirect_from_login_if_necessary # don't create new user or view form if user already exists
 
@@ -28,7 +28,7 @@ class UsersController < SessionsController
         cart.update_attributes(user: u)
       end
 
-      render status: 200, nothing: true, json: { redirectUrl: session[:previous_url] }
+      render status: 200, json: { redirectUrl: session[:previous_url] }
     end
   end
 
@@ -58,12 +58,10 @@ class UsersController < SessionsController
     token_uuid = params["token"]
     token = PasswordResetToken.find_by_uuid token_uuid
 
+    session[:user_uuid] = User.last.uuid
     if token && token.is_valid?
       token.update_users_password!(params["password"])
-      session[:user_uuid] = token.user.uuid
-      puts '!!!!!!!!!'
-      puts '!!!!!!!!!'
-      puts session[:user_uuid]
+      session[:user_uuid] = token.user_uuid
       render status: 200, body: nil
     else
       render status: 505, body: nil
