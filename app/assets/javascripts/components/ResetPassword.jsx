3b153571637f0a2b662a8bedb22172bc7ef7bb40
Sam E <sammy@Sams-MacBook-Pro-2.local>
2017-08-08 23:34:18 -0500

    redirect

--- app/assets/javascripts/components/ResetPassword.jsx
+++ app/assets/javascripts/components/ResetPassword.jsx
@@ -38,6 +38,9 @@ class ResetPassword extends React.Component {
       method: 'POST',
       url: '/reset_password',
       data: this.state,
+      success: function(response) {
+        window.location = '/'
+      }
     })
   }
 
