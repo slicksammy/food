372a048e71fccc1a58f422fdd12cbddd04b8ab14
Sam E <sammy@Sams-MacBook-Pro-2.local>
2017-08-10 17:26:09 -0500

    assets

--- public/assets/server_rendering-ca3d7eb16851121ec9c934d2250881241fc0cd1125e87bcbaa63c4df6b707815.js
+++ public/assets/server_rendering-ca3d7eb16851121ec9c934d2250881241fc0cd1125e87bcbaa63c4df6b707815.js
@@ -21124,6 +21124,68 @@ module.exports = function(reqctx) {
 /***/ })
 /******/ ]);
 });
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var Admin = (function (_React$Component) {
+  _inherits(Admin, _React$Component);
+
+  function Admin() {
+    _classCallCheck(this, Admin);
+
+    _get(Object.getPrototypeOf(Admin.prototype), "constructor", this).call(this);
+  }
+
+  _createClass(Admin, [{
+    key: "render",
+    value: function render() {
+      var orders = this.props.orders.map(function (order) {
+        return React.createElement(
+          "tr",
+          null,
+          React.createElement(
+            "td",
+            null,
+            order.expected_delivery_date
+          )
+        );
+      });
+
+      var headers = React.createElement(
+        "tr",
+        null,
+        React.createElement(
+          "th",
+          null,
+          "Expected Delivery Date"
+        )
+      );
+
+      console.log(this.props.orders);
+
+      var orders = this.props.orders.map(function (order) {
+        return React.createElement(
+          "div",
+          null,
+          React.createElement(Order, { order: order.order, items: order.items })
+        );
+      });
+
+      return React.createElement(
+        "div",
+        null,
+        orders
+      );
+    }
+  }]);
+
+  return Admin;
+})(React.Component);
 var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
 var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
