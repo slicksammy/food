175a9946af237de64e853862e164f49bcdb1c3ff
Sam E <sammy@Sams-MacBook-Pro-2.local>
2017-09-30 14:46:47 -0500

    better home screen

--- app/assets/javascripts/components/Home.jsx
+++ app/assets/javascripts/components/Home.jsx
@@ -21,7 +21,7 @@ class Home extends React.Component {
 
     return(
       <div>
-        { !orders ? <h1>No orders yet, let's <a href="/#store">shop!</a></h1> : orders }
+        { orders.length > 0 ? orders : <h1>No orders yet, let's <a href="/#store">shop!</a></h1> }
       </div>
     )
   }
