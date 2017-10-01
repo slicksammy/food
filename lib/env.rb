b8592b4cfb44655e719752ba561e8affdefc4eca
Sam E <sammy@Sams-MacBook-Pro-2.local>
2017-10-01 13:41:18 -0500

    ability to turn off orders

--- lib/env.rb
+++ lib/env.rb
@@ -1,11 +1,21 @@
 class Env
 
+  BOOLEAN_ENV_REGEX = /(true|1|yes)/i
+
+  def self.env_to_boolean(env)
+    return !!(env =~ BOOLEAN_ENV_REGEX)
+  end
+
   def self.base_uri
     Rails.env.production? ? 'http://www.iheartmeat.com' : 'http://www.localhost:3000'
   end
 
   def self.mailgun_api_key
     ENV['MAILGUN_API_KEY']
+  end
+
+  def self.accepting_orders?
+    env_to_boolean(ENV['ACCEPTING_ORDERS'])
   end
 
 end
