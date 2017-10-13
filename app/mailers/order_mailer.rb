f1502481ecc4cf218b47aae7b6e25de7450d8583
Sam E <sammy@Sams-MBP-2.attlocal.net>
2017-10-12 23:48:48 -0500

    send orders to staff at l&l

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -11,6 +11,7 @@ class OrderMailer < ActionMailer::Base
   SUPPORT_EMAIL = 'support@iheartmeat.com'
   SAM_EMAIL = 'sam@iheartmeat.com'
   JOEL_EMAIL = 'joel@worldsbeststeak.com'
+  L_L_EMAIL = 'orderdesk@worldsbeststeak.com'
 
   layout 'bootstrap'
 
@@ -26,7 +27,7 @@ class OrderMailer < ActionMailer::Base
   def place_order(orders=Order.paid.for_today)
     @orders = orders.map { |order| {order_number: order.order_number, items: to_string(::Checkout::OrderTotals.new(order: order).items)} }
 
-    mail(from: SAM_EMAIL, to: JOEL_EMAIL, cc: SAM_EMAIL, subject: "Orders for today")
+    mail(from: SAM_EMAIL, to: L_L_EMAIL , cc: [JOEL_EMAIL, SAM_EMAIL], subject: "Orders for Today")
   end
 
   def delivered(order, note: nil)
