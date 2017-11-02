2feed6a926287fbbbf183f6967814e8423bfce7d
Sam E <sammy@Sams-MBP-2.attlocal.net>
2017-11-01 20:09:44 -0500

    steak house price

--- app/assets/javascripts/components/Products.jsx
+++ app/assets/javascripts/components/Products.jsx
@@ -154,7 +154,7 @@ class Products extends React.Component {
             </div>*/}
             <h3>{product.description}</h3>
             <h2>${product.price}</h2>
-            <h3 style={restPrice}>Restaurant price: ${product.r_price}</h3>
+            <h3 style={restPrice}>Steak house price: ${product.r_price}</h3>
             { !this.props.showButtons ? null :
               <div>
                 <button style={buttonStyle} className="btn btn-success cart_update layer-shadow1 bright-green" onClick={()=> this.add(product.uuid, 1) } id="cart_add">+</button>
