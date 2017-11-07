995d63571b9b2ee37f0be2c94ba722421a4d3c80
Sam E <sammy@Sams-MBP-2.attlocal.net>
2017-11-07 12:42:14 -0600

    holidays

--- config/initializers/holidays.rb
+++ config/initializers/holidays.rb
@@ -1,11 +1,17 @@
-require 'holidays'
+# require 'holidays'
 
-start = Date.new(2016, 1, 1)
+# start = Date.new(2016, 1, 1)
 
-holidays = Holidays.between(start, 5.years.from_now, :us, :observed)
+# holidays = Holidays.between(start, 5.years.from_now, :us, :observed)
 
-holidays.each do |holiday|
-  BusinessTime::Config.holidays << holiday[:date]
-  # Implement long weekends:
-  BusinessTime::Config.holidays << holiday[:date].next_week if !holiday[:date].weekday?
+# holidays.each do |holiday|
+#   BusinessTime::Config.holidays << holiday[:date]
+#   # Implement long weekends:
+#   BusinessTime::Config.holidays << holiday[:date].next_week if !holiday[:date].weekday?
+# end
+
+HOLIDAYS = ["Thursday, November 23, 2017", "Monday, December 25, 2017", "January 1, 2017"]
+
+HOLIDAYS.each do |holiday|
+  BusinessTime::Config.holidays << holiday
 end
