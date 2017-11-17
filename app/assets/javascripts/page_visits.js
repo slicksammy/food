1f0cce979a119a9529c6a90420836c3020cc05dd
Sam E <sammy@Sams-MBP-2.attlocal.net>
2017-11-16 21:26:55 -0600

    ping server every 5 seconds for page visits

--- app/assets/javascripts/page_visits.js
+++ app/assets/javascripts/page_visits.js
@@ -1,6 +1,19 @@
 var href = window.location.href;
 var referrer = document.referrer;
+var defaultTimeout = 5000
 var pgid;
+
+function updateVisit(repeat) {
+  $.ajax({
+    method: 'POST',
+    url: '/update_page_visit',
+    data: { pgid: pgid }
+  });
+
+  if (repeat) {
+    setTimeout(function() {updateVisit(true)}.bind(this), defaultTimeout)
+  }
+}
 
 $(document).ready(function() {
   $.ajax({
@@ -9,16 +22,13 @@ $(document).ready(function() {
     data: { url: href, referrer: referrer },
     success: function(response) {
       pgid = response.pgid
+      setTimeout(function() {updateVisit(true)}.bind(this), defaultTimeout)
     }.bind(this)
   });
 });
 
 window.onbeforeunload = function() {
-  $.ajax({
-    method: 'POST',
-    url: '/update_page_visit',
-    data: { pgid: pgid }
-  });
+  updateVisit(false)
 };
 
 
