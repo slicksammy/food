2991d09323b46efeea2661aa83474a69269ca3c2
Sam E <sammy@Sams-MBP-2.attlocal.net>
2017-11-20 19:50:53 -0600

    your first order

--- app/helpers/promotion_helper.rb
+++ app/helpers/promotion_helper.rb
@@ -15,7 +15,7 @@ module PromotionHelper
     minimum_amount_string = ""
     amount = promotion.minimum_order.to_f.round(0)
 
-    minimum_amount_string = amount > 0 ? " any order of $#{amount.to_s} or more" : ""
+    minimum_amount_string = amount > 0 ? " your first order of $#{amount.to_s} or more" : ""
 
     "Shop at www.iheartmeat.com/steaks and use promo code #{promotion.code.upcase} to " + discount_string + minimum_amount_string + "."
   end
