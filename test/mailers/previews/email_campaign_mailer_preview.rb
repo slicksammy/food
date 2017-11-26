3244e6a0f1fc6fcbe4db182e5af98f0a5c2a792f
Sam E <sammy@Sams-MBP-2.attlocal.net>
2017-11-26 11:13:02 -0600

    new email

--- test/mailers/previews/email_campaign_mailer_preview.rb
+++ test/mailers/previews/email_campaign_mailer_preview.rb
@@ -1,6 +1,6 @@
 class EmailCampaignMailerPreview < ActionMailer::Preview
 
   def email_campaign
-    EmailCampaignMailer.email_campaign('november_19', { name: 'sam', to: 'sam@iheartmeat.com', subject: 'iheartmeat - skip the grocery store lines'})
+    EmailCampaignMailer.email_campaign('november_26', { name: 'sam', to: 'sam@iheartmeat.com', subject: 'iheartmeat - skip the grocery store lines'})
   end
 end
