f98e2adb12c266589b4b5ff76eded78172aafb26
Sam E <sammy@Sams-MacBook-Pro-2.local>
2017-12-08 10:59:37 -0600

    scope for received emails

--- app/models/email_log.rb
+++ app/models/email_log.rb
@@ -1,2 +1,3 @@
 class EmailLog < ActiveRecord::Base
+  scope :email_in_the_last_hours, ->(email, hours) { where("email = ? and created_at > current_timestamp - interval '? hours'", email, hours)}
 end
