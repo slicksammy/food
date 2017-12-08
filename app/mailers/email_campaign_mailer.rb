b160a815c989f7ec3d7361204b2a715c01d26723
Sam E <sammy@Sams-MacBook-Pro-2.local>
2017-12-08 11:09:33 -0600

    allow not to send too many emails

--- app/mailers/email_campaign_mailer.rb
+++ app/mailers/email_campaign_mailer.rb
@@ -4,7 +4,9 @@ class EmailCampaignMailer < ActionMailer::Base
 
   layout 'bootstrap'
 
-  def email_campaign(template, params)
+  def email_campaign(template, params, time_since_last_email=nil)
+    return if time_since_last_email && EmailLog.email_in_the_last_hours(params[:to], time_since_last_email).any?
+
     @params = params
 
     mail(to: params[:to], subject: params[:subject], bcc: params[:bcc], template_name: template)
